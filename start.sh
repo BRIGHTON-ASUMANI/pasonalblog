18dd28c5de3786cdbcf4d260d9d843398e553008
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-17 22:57:42 +0300

    heroku deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MAIL_USERNAME='asumanibrighton@gmail.com'
 export MAIL_PASSWORD='newa$um0ney'
+export DATABASE_URL='postgres://niploulydylefr:2221bcc015016020f8e471877bb960563148ead31696ee3341e9a51fc986eeed@ec2-54-221-237-246.compute-1.amazonaws.com:5432/ddhq2fgfqg3k4m'
+
 
 python3.6 manage.py server
