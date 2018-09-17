71005ef01c71a1853320e9b0d582b5c8081ab952
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-17 23:14:17 +0300

    new changes made in heroku

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     pass
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:newa$um0ney@localhost/blog_test'
